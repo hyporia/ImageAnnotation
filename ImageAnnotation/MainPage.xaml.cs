e409ed12b3638acadafc319761ad23755886fc35
MaxGalimov <maximgalimov96@gmail.com>
2020-08-12 01:55:29 +0300

    Добавил кнопки, открытие папки, перелистывание со стрелочек

--- ImageAnnotation/MainPage.xaml.cs
+++ ImageAnnotation/MainPage.xaml.cs
@@ -43,21 +43,29 @@ namespace ImageAnnotation
             picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
             picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
             picker.FileTypeFilter.Add(".jpg");
-
             var folder = await picker.PickSingleFolderAsync();
             if (folder != default(StorageFolder))
             {
-                Images = await folder.GetFilesAsync();
+				try
+                {
+                    LoadingIndicator.IsActive = true;
+                    Images = await folder.GetFilesAsync();
+				}
+				finally
+                {
+                    LoadingIndicator.IsActive = false;
+                }
 			}
         }
 
         private async Task SetImage(int index)
 		{
-            if (index > -1 && index < Images.Count)
+            if (Images != null && index > -1 && index < Images.Count)
 			{
                 this.image.Source = await GetImageFromFileAsync(Images[index]);
-			}
-		}
+                CurrentImageIndex = index;
+            }
+        }
 
         private async Task<BitmapImage> GetImageFromFileAsync(StorageFile file)
 		{
@@ -71,8 +79,37 @@ namespace ImageAnnotation
 
 		private async void Page_LoadedAsync(object sender, RoutedEventArgs e)
 		{
+
+		}
+
+		private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+		{
             await GetImages();
             await SetImage(0);
+        }
+
+		private async void buttonNext_ClickAsync(object sender, RoutedEventArgs e)
+		{
+            await SetImage(++CurrentImageIndex);
 		}
+
+		private async void buttonPrev_ClickAsync(object sender, RoutedEventArgs e)
+		{
+            await SetImage(--CurrentImageIndex);
+        }
+
+		private async void Grid_KeyUp(object sender, KeyRoutedEventArgs e)
+		{
+            switch (e.Key)
+            {
+                case Windows.System.VirtualKey.Right:
+                    buttonNext_ClickAsync(null, null);
+                    break;
+                case Windows.System.VirtualKey.Left:
+                    buttonPrev_ClickAsync(null, null);
+                    break;
+                default: break;
+            }
+        }
 	}
 }
